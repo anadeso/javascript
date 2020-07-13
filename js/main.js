fec047fd21477768b96750acd86fa67bb4b06c09
anadeso <anadeso90@gmail.com>
2020-07-13 17:29:05 -0300

    Implementacao Condicionais, laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
-var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+
+
+
+//var d = new Date();
+//alert(d);
+//alert(d.getMonth());
+//alert(d.getMonth() + 1);
+//alert(d.getDay());
+//alert(d.getHours());
+//alert(d.getFullYear());
+
+/*var count;
+for (count=0; count <= 5; count++){
+    console.log(count);
+};*/
+
+/*var count =0 
+
+while(count <= 5){
+    console.log(count);
+    count++;
+}*/
+
+
+/*var idade = prompt("Qual a sua idade");
+
+if (idade >= 18){
+    alert("Maior de idade");
+}else {
+    alert("Menor de idade");
+};*/
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
 console.log(frutas);
-alert(frutas[1].nome);
+alert(frutas[1].nome);*/
 
 // dicionario
 /*var fruta = {nome: "maça", cor: "vermelha"}
