0d3501e380e0fbd1317be2a19079df8ce534a8a0
anadeso <anadeso90@gmail.com>
2020-07-13 16:41:00 -0300

    Array e dicionario

--- js/main.js
+++ js/main.js
@@ -1,13 +1,50 @@
-var nome = "Ana Sousa";
-var idade = 29;
-var idade2 = 10;
-var frase = "Vamos entender isso"
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+// dicionario
+/*var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta);
+alert(fruta.nome);
+console.log(fruta.nome);
+alert(fruta.cor);
+console.log(fruta.cor);*/
+
+//var lista = ["maça", "pêra", "laranja"];
+// adiciona elemento da lista
+//lista.push("uva");
+// tira ultimo elemento da lista
+//lista.pop();
+
+// trazer elemento ao contrario da lista
+//console.log(lista.reverse());
+
+// toString
+//console.log(lista[0]);
+//console.log(lista.toString()[0  ]);
+
+// join lista e nao quero separar por , mas por - ou |
+//console.log(lista.join(" - "));
+
+
+
+// verifica o tamanho da lista
+//console.log(lista.length);
+//console.log(lista);
+//console.log(lista[1]);
+//alert(lista[1]);
+
+
+//var nome = "Ana Sousa";
+//ar idade = 29;
+//var idade2 = 10;
+//var frase = "Vamos entender isso"
 //alert(nome + " tem " + idade);
 //alert(idade + idade2);
 
-console.log(nome);
-console.log(idade);
+//console.log(nome);
+//console.log(idade);
 //console.log(frase.replace("isso", "aquilo"));
 //alert(frase.replace("isso", "aquilo"));
-console.log(frase.toLowerCase());
-console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+//console.log(frase.toUpperCase());
