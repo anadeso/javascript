cabb077a38a84f51e1a6b71cafbf770f38c1b054
anadeso <anadeso90@gmail.com>
2020-07-13 15:28:08 -0300

    Introducao JavaScript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var nome = "Ana Sousa";
+var idade = 29;
+var idade2 = 10;
+var frase = "Vamos entender isso"
+//alert(nome + " tem " + idade);
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade);
+//console.log(frase.replace("isso", "aquilo"));
+//alert(frase.replace("isso", "aquilo"));
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
